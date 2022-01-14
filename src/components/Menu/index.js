935b7ed683b9bbaa5a9b6e6b5af1032f1a15beac
renananiceto <renan.a.ferreira94@gmail.com>
2022-01-14 18:52:06 -0300

    update

---
+++ src/components/Menu/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function index() {
+  return (
+    <div>
+      <div>
+        <p>Renan Aniceto Ferreira</p>
+        <p>Renan Aniceto Ferreira</p>
+      </div>
+    </div>
+  )
+}
